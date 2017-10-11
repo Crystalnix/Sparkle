30ea76a7fcdaca7adcaffdd2ef77f9167ab0f58c
Vadim Zakondyrin <thekondr@crystalnix.com>
2017-10-11 11:37:01 +0600

    Revert logging implementation back to the first one
    We found that it would be hard for users to send us logs with the newest logging
    implementation in Sparkle, so we decided to revert it back.

--- Sparkle/SULog.m
+++ Sparkle/SULog.m
@@ -6,6 +6,10 @@
 //  Copyright © 2016 Sparkle Project. All rights reserved.
 //
 
+// The simplest way to disable original implementation and use our own while
+// simplifying merges with the latest changes from Sparkle is to use #if.
+#if 0
+
 #include "SULog.h"
 #include <asl.h>
 #include <os/log.h>
@@ -111,3 +115,78 @@ void SULog(SULogLevel level, NSString *format, ...)
         asl_send(client, message);
     });
 }
+
+#else
+// This is the first implementation of logs from Sparkle. They are now using
+// the implementation above but we found that it would be hard for users to
+// send us logs in case of any troubles, so we decided to revert the changes.
+
+// -----------------------------------------------------------------------------
+//	Headers:
+// -----------------------------------------------------------------------------
+
+#include "SULog.h"
+
+
+// -----------------------------------------------------------------------------
+//	Constants:
+// -----------------------------------------------------------------------------
+
+static NSString *const SULogFilePath = @"~/Library/Logs/SparkleUpdateLog.log";
+
+
+// -----------------------------------------------------------------------------
+//	SUClearLog:
+//		Erase the log at the start of an update. We don't want to litter the
+//		user's hard disk with logging data that's mostly unused, so each app
+//		should clear the log before it starts updating, so only the most recent
+//		update is kept around.
+//
+//	TAKES:
+//		sender	-	Object that sent this message, typically of type X.
+// -----------------------------------------------------------------------------
+
+void SUClearLog(void)
+{
+    FILE *logfile = fopen([[SULogFilePath stringByExpandingTildeInPath] fileSystemRepresentation], "w");
+    if (logfile) {
+        fclose(logfile);
+        SULog(@"===== %@ =====", [[NSFileManager defaultManager] displayNameAtPath:[[NSBundle mainBundle] bundlePath]]);
+    }
+}
+
+
+// -----------------------------------------------------------------------------
+//	SULog:
+//		Like NSLog, but logs to one specific log file. Each line is prefixed
+//		with the current date and time, to help in regressing issues.
+//
+//	TAKES:
+//		format	-	NSLog/printf-style format string.
+//		...		-	More parameters depending on format string's contents.
+// -----------------------------------------------------------------------------
+
+void SULog(SULogLevel level, NSString *format, ...)
+{
+    static BOOL loggedYet = NO;
+    if (!loggedYet) {
+        loggedYet = YES;
+        SUClearLog();
+    }
+
+    va_list ap;
+    va_start(ap, format);
+    NSString *theStr = [[NSString alloc] initWithFormat:format arguments:ap];
+    NSLog(@"Sparkle: %@", theStr);
+
+    FILE *logfile = fopen([[SULogFilePath stringByExpandingTildeInPath] fileSystemRepresentation], "a");
+    if (logfile) {
+        theStr = [NSString stringWithFormat:@"%@: %@\n", [NSDate date], theStr];
+        NSData *theData = [theStr dataUsingEncoding:NSUTF8StringEncoding];
+        fwrite([theData bytes], 1, [theData length], logfile);
+        fclose(logfile);
+    }
+    va_end(ap);
+}
+
+#endif
